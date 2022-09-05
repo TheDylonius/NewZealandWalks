804a23cb0549162089270e05fd4f2ad5639fa368
Dylonius <dylan.davis.uk@outlook.com>
2022-09-05 21:20:45 +0100

    End of Section 3

---
+++ NewZealandWalks/NZWalks.API/Controllers/RegionController.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using NZWalks.API.Repositories;
+
+namespace NZWalks.API.Controllers
+{
+    [ApiController]
+    [Route("regions")]
+    public class RegionController : Controller
+    {
+        private readonly IRegionRepository _regionRepository;
+        private readonly IMapper _mapper;
+
+        public RegionController(IRegionRepository regionRepository, IMapper mapper)
+        {
+            _regionRepository = regionRepository;
+            _mapper = mapper;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllRegions()
+        {
+            var regions = await _regionRepository.GetAllAsync();
+
+            /* Exchange domain model for DTO model.
+            var regionsDto = new List<Models.DTO.Region>();
+            
+            regions.ToList().ForEach(region =>
+            {
+                var regionDto = new Models.DTO.Region()
+                {
+                    Id = region.Id,
+                    Code = region.Code,
+                    Name = region.Name,
+                    Area = region.Area,
+                    Latitude = region.Latitude,
+                    Long = region.Long,
+                    Population = region.Population
+                };
+
+                regionsDto.Add(regionDto);
+            });
+            */
+
+            // Exchange domain model for DTO model via AutoMapper.
+            var regionsDto = _mapper.Map<List<Models.DTO.Region>>(regions);
+
+            return Ok(regionsDto);
+        }
+    }
+}
---
+++ NewZealandWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NewZealandWalksDbContext _dbContext;
+
+        public RegionRepository(NewZealandWalksDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return await _dbContext.Regions.ToListAsync();
+        }
+    }
+}
